078ae7414da1ae3db4c607d7fbb647d581b4bb05
Nikhil Arora <narora200@gmail.com>
2017-08-17 08:48:05 +0530

    back-end: prettify

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import {
+  graphqlExpress,
+  graphiqlExpress
+} from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
 
@@ -7,8 +10,12 @@ const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
+graphQLServer.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
