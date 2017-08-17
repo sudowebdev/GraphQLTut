3e2376960c311090c34359650a10f63e20948b8f
Nikhil Arora <narora200@gmail.com>
2017-08-17 08:53:25 +0530

    back-end: mocks.js comments added

--- back-end/data/mocks.js
+++ back-end/data/mocks.js
@@ -1,4 +1,7 @@
+// For mocking data when we don't have any data in the database
+
 const mocks = {
+  // If anyone asks for something of type String then run this function
   String: () => 'It works!',
 };
 
